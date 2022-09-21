1a61ab8158a9d943fdeceb0ab87070b5ec96ce62
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-21 19:16:21 -0300

    feat: add cors
    library responsable to let the frontend use the backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const loginRoute = require('../routes/login.route');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post('/login', loginRoute);
