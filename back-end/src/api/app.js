41aa9a5cf233892ee23d9b6748a1c6e5ce82e365
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-22 12:47:53 -0300

    feat: add imagens and a route images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/images', express.static('public/images'));
 app.use('/login', loginRoute);
 app.use('/users', userRoute);
 app.use('/products', [productRoute]);
