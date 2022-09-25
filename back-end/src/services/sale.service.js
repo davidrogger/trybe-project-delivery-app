a96659c04faeba9935f42d08a050e3125f09342e
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-25 11:34:43 -0300

    feat: add the products in the sale query
    to have knowledge of the product details in the sale

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -2,7 +2,18 @@ const model = require('../database/models');
 
 const saleService = {
   async getSalesByUserId(userId) {
-    return model.Sale.findAll({ where: { userId } });
+    return model.Sale.findAll({
+      where: { userId },
+      include: [
+        {
+          model: model.Product,
+          as: 'products',
+          attributes: { exclude: [] },
+          through: { attributes: ['quantity'] },
+        },
+      ],
+      attributes: { exclude: ['seller_id', 'user_id'] }, // tentando descobrir pq esses campos vem junto com userId e sellerId
+    });
   },
   async create(payload) {
     const { products, ...saleData } = payload;
