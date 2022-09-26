3e34802f304bc4eed97569c4bea3054f06e59d1e
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-26 18:32:37 -0300

    chore: remove strange floating component

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,16 +1,13 @@
 import Navbar from '../Components/Navbar';
 import ProductsDisplay from '../Components/ProductsDisplay';
 
-import { PageProducts as Page } from '../Components/ProductsCard/styles';
 import CheckoutButtom from '../Components/CheckoutButtom';
 
 function Products() {
   return (
     <>
       <Navbar />
-      <Page>
-        <ProductsDisplay />
-      </Page>
+      <ProductsDisplay />
       <CheckoutButtom />
     </>
   );
