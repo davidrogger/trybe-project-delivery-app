4e1654e38108a00337fd2b2b1d504eee34591b88
Gabriela Papin <gabrielapapin20@gmail.com>
2022-09-27 17:41:48 -0300

    chore: route sales
    added access and verify middleware

--- back-end/src/routes/sale.route.js
+++ back-end/src/routes/sale.route.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
 const saleController = require('../controllers/sale.controller');
-// const userController = require('../controllers/user.controller');
+const userController = require('../controllers/user.controller');
 
 const route = Router();
 
-// route.use(userController.verify);
 route.get('/:userId', saleController.getSalesByUser);
-route.post('/:userId', saleController.create);
+route.post('/:userId', userController.verify, saleController.create);
 
 module.exports = route;
