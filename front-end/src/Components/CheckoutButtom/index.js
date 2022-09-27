f78779f1f91a80a89a18566d68c65faf5350805f
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-27 11:49:13 -0300

    feat: created a price formatter

--- front-end/src/Components/CheckoutButtom/index.js
+++ front-end/src/Components/CheckoutButtom/index.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MyContext from '../../context/MyContext';
+import formatPrice from '../../utils/formatPrice';
 import { CartButtom } from './styles';
 
 function CheckoutButtom() {
@@ -18,11 +19,9 @@ function CheckoutButtom() {
           .find((product) => targetProduct.id === product.id); // Encontrando o produto para usar o preço para o calculo
         return (targetProduct.quantity * (Number(productFound.price) * 100)) / 100; // Para evitar float number multiplicando por 100
       })
-      .reduce((total, productTotal) => total + productTotal, 0) // soma todos produtos do carrinho ja multiplicados por seu preço
-      .toFixed(2) // para garantir que só serão exibidos 2 digitos após o ponto "."
-      .replace('.', ','); // teste espera encontrar virgula.
+      .reduce((total, productTotal) => total + productTotal, 0); // soma todos produtos do carrinho ja multiplicados por seu preço
 
-    setcartTotalValue(cartTotalPrice);
+    setcartTotalValue(formatPrice(cartTotalPrice));
   }, [products, cartProducts, setcartTotalValue]);
 
   return (
