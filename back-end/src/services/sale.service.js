41faaa5b8eb7a2859ed4f518b026dbda8014bfc8
Gabriela Papin <gabrielapapin20@gmail.com>
2022-09-28 17:55:27 -0300

    fix: Sale service
    delete include in getSalesByUserId

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -4,14 +4,6 @@ const saleService = {
   async getSalesByUserId(userId) {
     return model.Sale.findAll({
       where: { userId },
-      include: [
-        {
-          model: model.Product,
-          as: 'products',
-          attributes: { exclude: [] },
-          through: { attributes: ['quantity'] },
-        },
-      ],
       attributes: { exclude: ['seller_id', 'user_id'] }, // tentando descobrir pq esses campos vem junto com userId e sellerId
     });
   },
