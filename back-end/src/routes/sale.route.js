503d9f98f13810d76be4f0126f8184bda5e4a5aa
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-30 11:42:37 -0300

    fix: fix params from route to create a new order

--- back-end/src/routes/sale.route.js
+++ back-end/src/routes/sale.route.js
@@ -6,7 +6,7 @@ const route = Router();
 
 route.get('/customer/:id', saleController.getOrdersByUserId);
 route.get('/seller/:id', saleController.getOrdersBySellerId);
-route.post('/user/id', userController.verify, saleController.create);
+route.post('/user/:id', userController.verify, saleController.create);
 route.put('/:id', saleController.changeOrderStatus);
 route.get('/:id', saleController.getOrderById);
 
