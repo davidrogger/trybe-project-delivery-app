8b75aa0bc26bb4398b2ac8940457e6c39cfade7f
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-30 19:27:53 -0300

    style: changed cart button
    added a cart and a counter in the cart button

--- front-end/src/Components/CheckoutButtom/index.js
+++ front-end/src/Components/CheckoutButtom/index.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
 import MyContext from '../../context/MyContext';
 import * as style from './styles';
 import formatPrice from '../../utils/formatPrice';
@@ -16,9 +17,12 @@ function CheckoutButtom() {
       disabled={ cartProducts.length === 0 }
       onClick={ () => navigate('/customer/checkout') }
     >
-      Ver Carrinho
+      <div>
+        <ShoppingCart size={ 24 } />
+        {cartProducts.length}
+      </div>
       <style.CartButtonContent>
-        { 'R$: ' }
+        R$:
         <span data-testid="customer_products__checkout-bottom-value">
           { formatPrice(cartTotalValue) }
         </span>
--- front-end/src/Components/CheckoutButtom/styles.js
+++ front-end/src/Components/CheckoutButtom/styles.js
@@ -5,18 +5,22 @@ export const CartButton = styled.button`
   position: fixed;
   background: ${PRIMARY_COLOR};
   height: 80px;
-  width: 110px;
+  width: 130px;
   padding: 10px 20px;
   top: 60px;
   right: 10px;
-  border-radius: 10px;
+  border-radius: 20px 10px;
   color: white;
   border: none;
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
 export const CartButtonContent = styled.div`
   display: flex;
   width: 100%;
+  font-size: 1.5rem;
   justify-content: center;
   align-items: center;
   margin-top: 10px;
